7ae1d9bd5df7fabea9557739a70a66d94d0e9dce
Lindsay Gilbert <Lindsaygilbert722@gmail.com>
2017-02-09 16:36:31 -0800

    Added questions 6 and 7

--- app.js
+++ app.js
@@ -1,52 +1,115 @@
 'use strict';
 
-var confirmQuiz = prompt('Hello, would you like to take a quiz...About me? Please answer with Y or N.');
+// var confirmQuiz = prompt('Hello, would you like to take a quiz...About me? Please answer with Y or N.');
+//
+// if (confirmQuiz.toUpperCase() === 'Y'){
+//   alert('Cool! Here we go!');
+// } else {
+//   alert('Too bad, you are trapped now ha ha.');
+// }
+//
+// var user = prompt('First of all, what\'s your name?');
+// alert('Hello ' + user + ',nice to virtually meet you! Answer the following questions with Y or N');
+//
+// var questionOne = prompt('Okay, ' + user + ', first question: Do I have more than 20 tattoos?');
+//
+// if (questionOne.toUpperCase() === 'N'){
+//   alert('Correct! I have 19 tattoos');
+// } else {
+//   alert('False! I have 19');
+// }
+//
+// var questionTwo = prompt('Alright ' + user + ', second question: Was I of legal drinking age when I moved to Seattle?');
+//
+// if (questionTwo.toUpperCase() === 'Y'){
+//   alert('Yep, I was 23! You are correct');
+// } else {
+//   alert('YEAH! jk no, I was 23');
+// }
+//
+// var questionThree = prompt(user + ', third question: Do I have a cat?');
+//
+// if (questionThree.toUpperCase() === 'Y'){
+//   alert('Yep, I sure do.');
+// } else {
+//   alert('Wrong, I do.');
+// }
+//
+// var questionFour = prompt('Next question, ' + user + ', do I know how to ski?');
+//
+// if (questionFour.toUpperCase() === 'Y'){
+//   alert('Nope! I do know how to snowboard, though.');
+// } else {
+//   alert('Correct, I do not.');
+// }
+//
+// var questionFive = prompt('Final question! ' + user + ', is my birthday near Thanksgiving?');
+//
+// if (questionFive.toUpperCase() === 'Y'){
+//   alert('Wrong! It\'s in the spring.');
+// } else {
+//   alert('Correct, my birthday is in March.');
+// }
+//
+// for (var i = 0; i < 4; i++) {
+//   var questionSix = parseInt(prompt('Psyche! There are two more questions;). What is my favorite number? You have four tries'));
+//   if (questionSix === 22){
+//     alert('Yes! Correct!');
+//     break;
+//   } else if (questionSix > 22) {
+//     alert('You are too high.. Guess again.');
+//   } else if (questionSix < 22) {
+//     alert('You are too low.. Guess again.');
+//   } else {
+//     alert('Alright my favorite number is 22');
+//   }
+// }
 
-if (confirmQuiz.toUpperCase() === 'Y'){
-  console.log('Cool! Here we go!');
-} else {
-  console.log('Too bad, you are trapped now ha ha.');
+for (var i = 0; i < 6; i++) {
+  console.log(questionSeven);
+  var correctAnswers = ['ALASKA', 'COLORADO', 'OREGON', 'CALIFORNIA'];
+  var questionSeven = prompt('Last and final question, for real this time. What other state have I lived in besides Washington?');
+  if (correctAnswers.includes(questionSeven.toUpperCase()) === true) {
+    alert('Correct! I have lived in Washington, Alaska, Colorado, Oregon, and California.');
+    break;
+  } else {
+    alert('Guess again');
+  }
 }
 
-var user = prompt('First of all, what\'s your name?');
-console.log('Hello ' + user + ' ,nice to virtually meet you! Answer the following questions with Y or N');
 
-var questionOne = prompt('Okay, ' + user + ', first question: Do I have more than 20 tattoos?');
 
-if (questionOne.toUpperCase() === 'N'){
-  console.log('Correct! I have 19 tattoos');
-} else {
-  console.log('False! I have 19');
-}
 
-var questionTwo = prompt('Alright ' + user + ', second question: Was I of legal drinking age when I moved to Seattle?');
 
-if (questionTwo.toUpperCase() === 'Y'){
-  console.log('Yep, I was 23! You are correct');
-} else {
-  console.log('YEAH! jk no, I was 23');
-}
+// if (quizResponse === 'Y' && quizResponse === 'Yes') {
+//   console.log('Awesome!');
+// } else {
+//   console.log('TooBad');
+// }
 
-var questionThree = prompt(user + ', third question: Do I have a cat?');
+//var nestedThings = [['Adam', 'Amanda', 'Mika'], [30, 29, 6]];
 
-if (questionThree.toUpperCase() === 'Y'){
-  console.log('Yep, I sure do.');
-} else {
-  console.log('Wrong, I do.');
-}
+//for(var i = 0; nestedThingsIndex < nestedThings.length; nestedThingsIndex++) {
+//  console.log('index of loop:' + nestedThingsIndex);
+//  var nestedThing = nestedThing[];
 
-var questionFour = prompt('Next question, ' + user + ', do I know how to ski?');
+//  if (currentName === 'Amanda') {
+//    console.log('This is Amanda!!');
+//  }
+//  console.log('Name at index: ' + currentName);
+//}
 
-if (questionFour.toUpperCase() === 'Y'){
-  console.log('Nope! I do know how to snowboard, though.');
-} else {
-  console.log('Correct, I do not.');
-}
 
-var questionFive = prompt('Final question! ' + user + ',is my birthday near Thanksgiving?');
-
-if (questionFive.toUpperCase() === 'Y'){
-  console.log('Wrong! It\'s in the spring.');
-} else {
-  console.log('Correct, my birthday is in March.');
-}
+// var i = 0;
+// while (i < names.length) {
+//   for(var index = 0; i < names.length; i++) {
+//    console.log('index of loop:' + i);
+//    var currentName = names[i];
+//
+//    if (currentName === 'Amanda') {
+//      console.log('This is Amanda!!');
+//    }
+//    console.log('Name at index: ' + names[i]);
+//   }
+//   i++
+// }
